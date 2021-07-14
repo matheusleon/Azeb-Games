d9c67d74bf57b27940e83f94b5ce6e64b9623aa8
mhco0 <mhco@cin.ufpe.br>
2021-07-14 04:08:04 -0300

    Adding AirConsole plugin, but still some problem on socket

--- Azeb Games/Assets/PlayerMovement.cs
+++ Azeb Games/Assets/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +13,16 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
 
     bool jump = false;
+
+    void Awake() 
+    {
+        AirConsole.instance.onMessage += onMessage;
+    }
+
+    void onMessage(int fromDevice, JToken data)
+    {
+        Debug.Log("message from " + fromDevice + ", data: " + data);
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,5 +39,12 @@ public class PlayerMovement : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump); 
         jump = false;
+    }
+
+    void onDestroy()
+    {
+        if (AirConsole.instance != null){
+            AirConsole.instance.onMessage -= onMessage;
+        }
     }
 }
