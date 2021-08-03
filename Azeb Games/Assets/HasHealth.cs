6f5c51b0c865696ec9bb4502e3e879063ac83133
vserraa <vssm@cin.ufpe.br>
2021-08-02 21:27:42 -0300

    adding game over screens

--- Azeb Games/Assets/HasHealth.cs
+++ Azeb Games/Assets/HasHealth.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HasHealth : MonoBehaviour
 {
     public float health;
+    public bool triggersGameOver = false;
+    public Team team;
+
+    public enum Team {
+        Blue,
+        Red
+    }
+
     // Start is called before the first frame update
     public void TakeDamage(int damage) {
     	health -= damage;
@@ -15,5 +24,16 @@ public class HasHealth : MonoBehaviour
     		Destroy(gameObject);
             Debug.Log("Game object died");
     	}
+    }
+
+    public void OnDestroy() {
+        if (triggersGameOver) {
+            string sceneName = null;
+            if (this.team == Team.Blue)
+                sceneName = "GameOverRedWins";            
+            if (this.team == Team.Red)
+                sceneName = "GameOverBlueWins";
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
     }
 }
