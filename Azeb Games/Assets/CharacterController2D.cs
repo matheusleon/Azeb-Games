4b98e559365d54d360b39719bd3c80ff704888a1
Matheus Leon <mlan@cin.ufpe.br>
2021-08-09 23:58:40 -0300

    Start ranged attack

--- Azeb Games/Assets/CharacterController2D.cs
+++ Azeb Games/Assets/CharacterController2D.cs
@@ -109,12 +109,8 @@ public class CharacterController2D : MonoBehaviour
 
 	private void Flip()
 	{
-		// Switch the way the player is labelled as facing.
 		m_FacingRight = !m_FacingRight;
 
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+		transform.Rotate(0f, 180f, 0f);
 	}
 }
--- Azeb Games/Assets/PlayerSpawner.cs
+++ Azeb Games/Assets/PlayerSpawner.cs
@@ -31,9 +31,6 @@ public class PlayerSpawner : MonoBehaviour
         {
 			player_instance.GetComponent<PlayerMovement>().id = this.id;
 			player_instance.GetComponent<MeleeAttack>().id = this.id;
-
-			Debug.Log("Setando player = " + playerName);
-			Debug.Log(player_instance.GetComponent<PlayerMovement>().id + " O QUE = " + this.id);
 		}
 
 	}
