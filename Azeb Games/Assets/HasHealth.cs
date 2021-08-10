2c39b3eaef64692ebae6a348c305b75d579d8e88
José Victor Cruz <jvsc@cin.ufpe.br>
2021-08-10 03:44:46 -0300

    feat(Bots): initial animation state machine

--- Azeb Games/Assets/HasHealth.cs
+++ Azeb Games/Assets/HasHealth.cs
@@ -9,21 +9,45 @@ public class HasHealth : MonoBehaviour
     public bool triggersGameOver = false;
     public Team team;
 
+    public Animator animator;
+    private bool died = false;
+
     public enum Team {
         Blue,
         Red
     }
+
+    public static bool HasParameter(string paramName, Animator animator)
+    {
+        foreach (AnimatorControllerParameter param in animator.parameters)
+        {
+            if (param.name == paramName)
+                return true;
+        }
+        return false;
+    }
+
 
     // Start is called before the first frame update
     public void TakeDamage(int damage) {
     	health -= damage;
     	
         Debug.Log("damage taken");
-        
-        if (health <= 0) {
-    		Destroy(gameObject);
+
+        if (health <= 0 && !died)
+        {
+            died = true;
+            Destroy(gameObject.GetComponent<BoxCollider2D>());
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+            float dyingTime = 0;
+            if (HasParameter("Dying", animator))
+            {
+                animator.SetTrigger("Dying");
+                dyingTime = 1.5f;
+            }
             Debug.Log("Game object died");
-    	}
+            Destroy(gameObject, dyingTime);
+        }
     }
 
     public void OnDestroy() {
