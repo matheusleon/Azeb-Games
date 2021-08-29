9e9b813135c23ffa85b3bac5a907859c5bf44cc1
Gabriel Mendes <ggml@cin.ufpe.br>
2021-08-28 23:40:56 -0300

    feat: refactor and adjust menu design and transitions

--- Azeb Games/Assets/LobbyPlayerConnector.cs
+++ Azeb Games/Assets/LobbyPlayerConnector.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 using NDream.AirConsole;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Newtonsoft.Json.Linq;
 
 public class LobbyPlayerConnector : MonoBehaviour
 {
     public static int PLAYER_LEFT_ID = -1;
     public static int PLAYER_RIGHT_ID = -1;
-    public static Button PLAYER_LEFT_BUTTON;
-    public static Button PLAYER_RIGHT_BUTTON;
+    public static Image PLAYER_LEFT_IMAGE;
+    public static Image PLAYER_RIGHT_IMAGE;
+    public static bool LOBBY_ACTIVE = false;
 
     void Start()
     {
-        AirConsole.instance.onConnect += this.onConnect;
+        AirConsole.instance.onMessage += this.onMessage;
         DontDestroyOnLoad(this.gameObject);
         
-        GameObject playerLeftBtn = GameObject.Find("Player1JoinButton");
-        if (playerLeftBtn != null) {
-            LobbyPlayerConnector.PLAYER_LEFT_BUTTON = playerLeftBtn.GetComponent<Button>();
+        GameObject playerLeftImg = GameObject.Find("Player1JoinButton");
+        if (playerLeftImg != null) {
+            LobbyPlayerConnector.PLAYER_LEFT_IMAGE = playerLeftImg.GetComponent<Image>();
         }
-        GameObject playerRightBtn = GameObject.Find("Player2JoinButton");
-        if (playerRightBtn != null) {
-            LobbyPlayerConnector.PLAYER_RIGHT_BUTTON = playerRightBtn.GetComponent<Button>();
+        GameObject playerRightImg = GameObject.Find("Player2JoinButton");
+        if (playerRightImg != null) {
+            LobbyPlayerConnector.PLAYER_RIGHT_IMAGE = playerRightImg.GetComponent<Image>();
         }
+
+        LobbyPlayerConnector.LOBBY_ACTIVE = true;
     }
 
-    public static void updateButtonColor(Button button, Color color) {
-        if (button != null) {
-            var colors = button.colors;
-            colors.normalColor = color;
-            button.colors = colors;
+    void Awake()
+    {
+        LobbyPlayerConnector.LOBBY_ACTIVE = true;
+    }
+
+    public static void updateButtonColor(Image image, Color color) {
+        if (image != null) {
+            image.color = color;
+        } else {
+            Debug.Log("nunca que acha a imagem");
         }
     }
 
@@ -39,16 +48,19 @@ public class LobbyPlayerConnector : MonoBehaviour
         if (LobbyPlayerConnector.PLAYER_LEFT_ID == -1)
         {
             LobbyPlayerConnector.PLAYER_LEFT_ID = deviceId;
-            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_LEFT_BUTTON, Color.green);
+            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_LEFT_IMAGE, Color.green);
         }
         else if (LobbyPlayerConnector.PLAYER_RIGHT_ID == -1)
         {
             LobbyPlayerConnector.PLAYER_RIGHT_ID = deviceId;
-            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_RIGHT_BUTTON, Color.green);
+            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_RIGHT_IMAGE, Color.green);
+        } else {
+            Debug.Log("entrei em lugar nenhum doido");
         }
 
         if (LobbyPlayerConnector.PLAYER_LEFT_ID != -1 && LobbyPlayerConnector.PLAYER_RIGHT_ID != -1) {
             Debug.Log("começando jogo");
+            LobbyPlayerConnector.LOBBY_ACTIVE = false;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
@@ -57,47 +69,58 @@ public class LobbyPlayerConnector : MonoBehaviour
         if (LobbyPlayerConnector.PLAYER_LEFT_ID == deviceId)
         {
             LobbyPlayerConnector.PLAYER_LEFT_ID = -1;
-            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_LEFT_BUTTON, Color.red);
+            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_LEFT_IMAGE, Color.red);
         }
         else if (LobbyPlayerConnector.PLAYER_RIGHT_ID == deviceId)
         {
             LobbyPlayerConnector.PLAYER_RIGHT_ID = -1;
-            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_RIGHT_BUTTON, Color.red);
+            LobbyPlayerConnector.updateButtonColor(LobbyPlayerConnector.PLAYER_RIGHT_IMAGE, Color.red);
         }
     }
 
-    void onConnect(int deviceId)
-    {
-        Debug.Log("CONECTOU NO LOBBY " + deviceId.ToString());
-        if (LobbyPlayerConnector.PLAYER_LEFT_ID == -1)
-        {
-            LobbyPlayerConnector.PLAYER_LEFT_ID = deviceId;
-        }
-        else if (LobbyPlayerConnector.PLAYER_RIGHT_ID == -1)
-        {
-            LobbyPlayerConnector.PLAYER_RIGHT_ID = deviceId;
-        }
+    bool hasPlayerJoined(int deviceId) {
+        return LobbyPlayerConnector.PLAYER_LEFT_ID == deviceId || LobbyPlayerConnector.PLAYER_RIGHT_ID == deviceId;
+    }
 
-        if (LobbyPlayerConnector.PLAYER_LEFT_ID != -1 && LobbyPlayerConnector.PLAYER_RIGHT_ID != -1) {
-            Debug.Log("começando jogo");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    void onMessage(int device_id, JToken data)
+    {
+        if (!LobbyPlayerConnector.LOBBY_ACTIVE) {
+            return;
+        }
+        Debug.Log("RECEBI NO LOBBY: " + device_id.ToString());
+        Debug.Log(data);
+
+        string element = data.Value<string>("element");
+        JObject parsedData = data.Value<JObject>("data");
+
+        if (element.ToString() == "attackButton") {
+            var pressed = (int) parsedData["pressed"];
+            if (pressed == 0) {
+                return;
+            }
+            if (this.hasPlayerJoined(device_id)) {
+                this.onPlayerExit(device_id);
+            } else {
+                this.onPlayerJoin(device_id);
+            }
+        } else if (element.ToString() == "special") {
+            // Go back to main menu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
 
-    void onDisconnect(int deviceId)
+    void onDisable()
     {
-        Debug.Log("DEVICE " + deviceId.ToString() + " SAIU DO LOBBY");
-        if (LobbyPlayerConnector.PLAYER_LEFT_ID == deviceId) {
-            LobbyPlayerConnector.PLAYER_LEFT_ID = -1;
-        } else {
-            LobbyPlayerConnector.PLAYER_RIGHT_ID = -1;
+        if (AirConsole.instance != null) {
+            Debug.Log("tirei o onMessage do lobby");
+            AirConsole.instance.onMessage -= this.onMessage;
         }
     }
 
     void onDestroy()
     {
         if (AirConsole.instance != null) {
-            AirConsole.instance.onConnect -= this.onConnect;
+            AirConsole.instance.onMessage -= this.onMessage;
         }
     }
 }
